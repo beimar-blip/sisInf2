9b981cf783b1601ded419dd9cb7e8a68fc7d79aa
shizukani9 <73003469+shizukani9@users.noreply.github.com>
2020-10-20 14:01:49 -0400

    Update ProyectoSI.java

--- ProyectoSI/src/proyectosi/ProyectoSI.java
+++ ProyectoSI/src/proyectosi/ProyectoSI.java
@@ -1,11 +1,12 @@
-
+import javax.swing.*;
 public class ProyectoSI {
 
     /**
-     * @param args the command line arguments
+     * 
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "Tatiana");
     }
     
 }

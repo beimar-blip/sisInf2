4e40c713e03cab0c3e900b1a4e809f1be527477c
Beimar Colque <beimarcolque987@gmail.com>
2020-10-22 14:38:29 -0400

    hola mens

---
+++ hula/src/hula/Hula.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hula;
+
+/**
+ *
+ * @author Beimar
+ */
+public class Hula {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.err.println("holas");
+        // TODO code application logic here
+    }
+    
+}
